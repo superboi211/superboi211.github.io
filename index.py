6b44fc17441fcf0712f513aecf012da43b7fb782
Bless B <eyronbler@gmail.com>
2022-09-01 17:58:51 -0400

    Update index.py

--- index.py
+++ index.py
@@ -1,8 +1,11 @@
 from browser import document
 from browser.widgets.dialog import InfoDialog
 
-def click(ev):
+def define_vars():
     global test
+
+def click(ev):
+    define_vars()
     test += 1
     if document['zone'].value == '':
         InfoDialog('Hey!', 'Please remember to enter a name!')
