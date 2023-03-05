0f6f65a250e00c790d4024c9472280a1bb48fbec
Bless B <eyronbler@gmail.com>
2023-03-04 21:16:42 -0500

    sitelen pona thing 1.1

--- sitelenpona/script.js
+++ sitelenpona/script.js
@@ -2,6 +2,6 @@ function darkMode() {
     var element = document.body;
     element.classList.toggle("darkMode");
       
-  }
+}
   
-  document.getElementById('dark').addEventListener("click",darkMode);
+document.getElementById('dark').addEventListener("click",darkMode);
